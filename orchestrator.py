df563425631d0a0ca079701204728ce215a0def1
tritant <tritant@hotmail.com>
2025-09-23 15:53:56 +0200

    Add files via upload

--- orchestrator.py
+++ orchestrator.py
@@ -1,21 +1,3 @@
-class OrchestratorNode:
-    NAME = "OrchestratorNode"
-    DISPLAY_NAME = "Orchestrator (Workflow Switch)"
-    FUNCTION = "do_nothing"
-    CATEGORY = "Logic"
-    
-    @classmethod
-    def INPUT_TYPES(cls):
-        # Le widget est maintenant 100% géré par le JavaScript.
-        return { "required": {} }
-
-    RETURN_TYPES = ()
-    OUTPUT_NODE = True
-
-    # La fonction n'a plus besoin d'argument.
-    def do_nothing(self):
-        return ()
-        
 class OrchestratorNodeMuter:
     NAME = "OrchestratorNode autoconfig"
     DISPLAY_NAME = "Orchestrator (auto config)"
@@ -35,5 +17,5 @@ class OrchestratorNodeMuter:
         return ()        
 
 # Ces dictionnaires restent inchangés
-NODE_CLASS_MAPPINGS = { "OrchestratorNode": OrchestratorNode, "OrchestratorNodeMuter": OrchestratorNodeMuter }
-NODE_DISPLAY_NAME_MAPPINGS = { "OrchestratorNode": "OrchestratorNode", "OrchestratorNodeMuter": "OrchestratorNode autoconfig" }
+NODE_CLASS_MAPPINGS = { "OrchestratorNodeMuter": OrchestratorNodeMuter }
+NODE_DISPLAY_NAME_MAPPINGS = { "OrchestratorNodeMuter": "OrchestratorNode autoconfig" }
