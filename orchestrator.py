e897344edfe69c86d4d0fb60c71f6512d25a52b4
tritant <tritant@hotmail.com>
2025-09-23 11:17:53 +0200

    Add muter

--- orchestrator.py
+++ orchestrator.py
@@ -15,7 +15,25 @@ class OrchestratorNode:
     # La fonction n'a plus besoin d'argument.
     def do_nothing(self):
         return ()
+        
+class OrchestratorNodeMuter:
+    NAME = "OrchestratorNode Muter"
+    DISPLAY_NAME = "Orchestrator (Workflow Switch)"
+    FUNCTION = "do_nothing"
+    CATEGORY = "Logic"
+    
+    @classmethod
+    def INPUT_TYPES(cls):
+        # Le widget est maintenant 100% géré par le JavaScript.
+        return { "required": {} }
+
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
+
+    # La fonction n'a plus besoin d'argument.
+    def do_nothing(self):
+        return ()        
 
 # Ces dictionnaires restent inchangés
-NODE_CLASS_MAPPINGS = { OrchestratorNode.NAME: OrchestratorNode }
-NODE_DISPLAY_NAME_MAPPINGS = { OrchestratorNode.NAME: OrchestratorNode.DISPLAY_NAME }
+NODE_CLASS_MAPPINGS = { "OrchestratorNode": OrchestratorNode, "OrchestratorNodeMuter": OrchestratorNodeMuter }
+NODE_DISPLAY_NAME_MAPPINGS = { "OrchestratorNode": "OrchestratorNode", "OrchestratorNodeMuter": "OrchestratorNode Muter" }
