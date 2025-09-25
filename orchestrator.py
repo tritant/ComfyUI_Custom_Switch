ffe63d85193d7590a9527b9500f47b3ff3b49825
tritant <tritant@hotmail.com>
2025-09-25 18:14:52 +0200

    add group bypass/mute

--- orchestrator.py
+++ orchestrator.py
@@ -30,5 +30,47 @@ class OrchestratorNodeToogle:
     def do_nothing(self):
         return ()        
 
-NODE_CLASS_MAPPINGS = { "OrchestratorNodeToogle": OrchestratorNodeToogle, "OrchestratorNodeMuter": OrchestratorNodeMuter }
-NODE_DISPLAY_NAME_MAPPINGS = { "OrchestratorNodeToogle": "OrchestratorNode Bypass autoconfig", "OrchestratorNodeMuter": "OrchestratorNode Muter autoconfig" }
+class OrchestratorNodeGroupBypasser:
+    NAME = "OrchestratorNode Group Bypasser"
+    DISPLAY_NAME = "Orchestrator Group Bypasser"
+    FUNCTION = "do_nothing"
+    CATEGORY = "Logic"
+    
+    @classmethod
+    def INPUT_TYPES(cls):
+        return { "required": {} }
+
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
+
+    def do_nothing(self):
+        return ()
+        
+class OrchestratorNodeGroupMuter:
+    NAME = "OrchestratorNode Group Muter"
+    DISPLAY_NAME = "Orchestrator Group Muter"
+    FUNCTION = "do_nothing"
+    CATEGORY = "Logic"
+    
+    @classmethod
+    def INPUT_TYPES(cls):
+        return { "required": {} }
+
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
+
+    def do_nothing(self):
+        return ()
+        
+NODE_CLASS_MAPPINGS = { 
+    "OrchestratorNodeToogle": OrchestratorNodeToogle,
+    "OrchestratorNodeMuter": OrchestratorNodeMuter,
+    "OrchestratorNodeGroupBypasser": OrchestratorNodeGroupBypasser,
+    "OrchestratorNodeGroupMuter": OrchestratorNodeGroupMuter
+    }
+NODE_DISPLAY_NAME_MAPPINGS = { 
+    "OrchestratorNodeToogle": "OrchestratorNode Bypass autoconfig",
+    "OrchestratorNodeMuter": "OrchestratorNode Muter autoconfig",
+    "OrchestratorNodeGroupBypasser": "Orchestrator Group Bypasser",
+    "OrchestratorNodeGroupMuter": "Orchestrator Group Muter"
+    }
