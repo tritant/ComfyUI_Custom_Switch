b4ca5d916ddd975743cc09c79defb8eb14228f42
tritant <tritant@hotmail.com>
2025-10-07 22:10:14 +0200

    work with subgraph

--- js/orchestratortoogle.js
+++ js/orchestratortoogle.js
@@ -1,8 +1,21 @@
 import { app } from "/scripts/app.js";
 
+function getAllNodesInGraph() {
+    const allNodes = [...app.graph._nodes];
+    
+    if (app.graph._subgraphs) {
+        for (const subgraph of app.graph._subgraphs.values()) {
+            //console.log(`[Orchestrator] Trouvé un Subgraph. Scan de ses ${subgraph.nodes.length} nœuds internes...`);
+            allNodes.push(...subgraph.nodes);
+        }
+    }
+    return allNodes;
+}
+
 function updateNodeStates(selectedTag, groupID, workflowTags) {
     const changesToApply = [];
-    for (const targetNode of app.graph.nodes) {
+    const dirtyGraphs = new Set([app.graph]);
+    for (const targetNode of getAllNodesInGraph()) {
         if (targetNode.type === "OrchestratorNodeToogle") continue;
         const title = targetNode.title || "";
         const match = title.match(/\[\[(.*?):(.*?)\]\]/g);
@@ -15,6 +28,9 @@ function updateNodeStates(selectedTag, groupID, workflowTags) {
                     const newMode = (selectedTag && nodeTag === selectedTag) ? 0 : 4;
                     if (targetNode.mode !== newMode) {
                         changesToApply.push({ node: targetNode, mode: newMode });
+                        if (targetNode.graph) {
+                            dirtyGraphs.add(targetNode.graph);
+                        }
                     }
                     break;
                 }
@@ -25,12 +41,14 @@ function updateNodeStates(selectedTag, groupID, workflowTags) {
         for (const change of changesToApply) {
             change.node.mode = change.mode;
         }
-        app.graph.setDirtyCanvas(true, true);
+        for (const graph of dirtyGraphs) {
+            graph.setDirtyCanvas(true, true);
+        }
     }
 }
 
 app.registerExtension({
-    name: "Comfy.OrchestratorNodeToogle.Final.Flexible",
+    name: "Comfy.OrchestratorNodeToogle.TheRealFinalVersion",
 
     beforeRegisterNodeDef(nodeType, nodeData, app) {
         if (nodeData.name === "OrchestratorNodeToogle") {
@@ -43,32 +61,18 @@ app.registerExtension({
                 const discoverAndBuildUI = () => {
                     const groupIDWidget = this.widgets.find(w => w.name === "group_id");
                     if (!groupIDWidget) return;
-
                     const groupID = groupIDWidget.value;
                     if (!groupID) return;
 
-                    let activeTagInGraph = null;
-                    const regexForScan = /\[\[(.*?):(.*?)\]\]/g;
-                    for (const node of app.graph.nodes) {
-                        if (node.mode === 0 && node.title) {
-                            for (const match of node.title.matchAll(regexForScan)) {
-                                if (match[1] === groupID) {
-                                    activeTagInGraph = match[2];
-                                    break;
-                                }
-                            }
-                        }
-                        if (activeTagInGraph) break;
-                    }
+                    const activeWidget = this.widgets.find(w => w.type === 'toggle' && w.value);
+                    const activeTagName = activeWidget ? activeWidget.name : null;
                     
                     this.widgets = this.widgets.filter(w => w.name === "group_id");
                     
                     const discoveredTags = new Set();
-                    const regex = /\[\[(.*?):(.*?)\]\]/g;
-
-                    for (const node of app.graph.nodes) {
+                    for (const node of getAllNodesInGraph()) {
                         if (node.title) {
-                            const allMatches = [...node.title.matchAll(regex)];
+                            const allMatches = [...node.title.matchAll(/\[\[(.*?):(.*?)\]\]/g)];
                             for (const match of allMatches) {
                                 if (match[1] === groupID) {
                                     discoveredTags.add(match[2]);
@@ -90,7 +94,6 @@ app.registerExtension({
 
                     const handleToggleClick = (toggledTagName, isTurningOn) => {
                         let newSelectedTag = isTurningOn ? toggledTagName : null;
-
                         if (isTurningOn) {
                             for (const w of this.widgets) {
                                 if (WORKFLOW_TAGS.includes(w.name) && w.name !== toggledTagName) {
@@ -98,39 +101,38 @@ app.registerExtension({
                                 }
                             }
                         }
-                        
                         updateNodeStates(newSelectedTag, groupID, WORKFLOW_TAGS);
                     };
 
                     for (const tag of WORKFLOW_TAGS) {
-                        this.addWidget("toggle", tag, false, (value) => { handleToggleClick(tag, value); });
+                        const isEnabled = (tag === activeTagName);
+                        this.addWidget("toggle", tag, isEnabled, (value) => { handleToggleClick(tag, value); });
                     }
 
-                    if (activeTagInGraph) {
-                        const widgetToActivate = this.widgets.find(w => w.name === activeTagInGraph);
-                        if (widgetToActivate) {
-                            widgetToActivate.value = true;
-                        }
+                    const anyTogglesOn = this.widgets.some(w => w.type === 'toggle' && w.value);
+                    if (WORKFLOW_TAGS.length > 0 && !anyTogglesOn) {
+                        const firstToggle = this.widgets.find(w => w.type === 'toggle');
+                        if(firstToggle) firstToggle.value = true;
                     }
                     
+                    updateNodeStates(this.widgets.find(w => w.type === 'toggle' && w.value)?.name || null, groupID, WORKFLOW_TAGS);
+
                     const currentWidth = this.size[0];
                     const newComputedSize = this.computeSize();
-                    this.size = [currentWidth, newComputedSize[1]];
-                    
+                    this.size = [Math.max(currentWidth, newComputedSize[0]), newComputedSize[1]];
                     app.graph.setDirtyCanvas(true, true);
                 };
 
                 const groupIDWidget = this.addWidget("STRING", "group_id", "DEFAULT", discoverAndBuildUI);
                 
-                discoverAndBuildUI();
+                setTimeout(() => discoverAndBuildUI(), 100);
             };
 
             const onDrawBackground = nodeType.prototype.onDrawBackground;
             nodeType.prototype.onDrawBackground = function(ctx) {
                 onDrawBackground?.apply(this, arguments);
-
                 let titlesSignature = "";
-                for(const node of app.graph.nodes) {
+                for(const node of getAllNodesInGraph()) {
                     if(node.title?.includes('[[')) {
                         titlesSignature += node.title;
                     }
